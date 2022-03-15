328da479b2264bc1a8e39e8e8ae19e22182c1433
Mrsazza <shahriarhossain147@gmail.com>
2022-03-16 05:38:33 +0600

    Added 2 list to check difference

--- NewDayNewQuote/ContentView.swift
+++ NewDayNewQuote/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    var quote = ["Hello","world","Hola"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Shuffled")
+            List{
+                ForEach(isNewDay() ? quote.shuffled() : quote,id:\.self){ quot in
+                    Text(quot)
+                }
+            }
+            Text("Not Shuffled")
+            List{
+                ForEach(quote,id:\.self){ quot in
+                    Text(quot)
+                }
+            }
+        }
+    }
+}
+
+func isNewDay()-> Bool{
+    
+    let currentDate = Date()
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MM/dd/yyyy"
+    
+    let currentDateString = dateFormatter.string(from: currentDate)
+    
+    if let lastSaved  = UserDefaults.standard.string(forKey: "lastDate"){// last saved date
+        
+        
+        if lastSaved == currentDateString{
+            return true
+        }else{
+            
+            UserDefaults.standard.setValue(currentDateString, forKey: "lastDate")
+            return false
+            
+        }
+    }else{
+        
+        UserDefaults.standard.setValue(currentDateString, forKey: "lastDate")
+        return false
     }
 }
 

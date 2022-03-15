71ab6f164be038e77b61547c28e26cb31347c548
Mrsazza <shahriarhossain147@gmail.com>
2022-03-16 05:17:29 +0600

    Initial Commit

---
+++ NewDayNewQuote/NewDayNewQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDayNewQuoteApp.swift
+//  NewDayNewQuote
+//
+//  Created by Sazza on 16/3/22.
+//
+
+import SwiftUI
+
+@main
+struct NewDayNewQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
